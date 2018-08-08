5a7cdc0491569797cee43a2905069f542a833454
healerhe <1911978311@qq.com>
2018-08-08 15:40:56 +0800

    test

---
+++ src/main/java/HelloGitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: healer
+ * @create: 2018/8/8
+ **/
+public class HelloGitTest {
+    @Test
+    public void test(){
+        System.out.println("helloGit");
+    }
+}
